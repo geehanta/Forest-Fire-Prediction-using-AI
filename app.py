ed651b3c8f49becc460758d8e71e687b3c4fc1a8
Geehanta <hunterslenses@gmail.com>
2023-08-24 16:10:45 +0300

    added jupyter notebook

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
-from flask import Flask,request, url_for, redirect, render_template
+
+from flask import Flask, request, render_template
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-model=pickle.load(open('model.pkl','rb'))
-
+model = pickle.load(open('rfmodel.pkl', 'rb'))
+selected_symptoms = []  # Initialize `selected_symptoms`
 
 @app.route('/')
 def hello_world():
-    return render_template("forest_fire.html")
+    return render_template("pathogen_predict.html")
 
-
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
-    print(int_features)
-    print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
+    global selected_symptoms, prediction_made
+    selected_symptoms = request.form.getlist('symptoms')
 
-    if output>str(0.5):
-        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
+    if not selected_symptoms:
+        out = "No prediction (no symptoms selected)"
     else:
-        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
+        all_symptoms = ["Chills", "Cough", "Difficulty_breathing", "Sputum_production", "Sore_throat", "Headache", "Runny_nose", "Eye_pain", "Seizures", "Tick_bites", 
+                        "Abdominal_pain", "Vomiting", "Diarrhoea", "Blood_in_stool", "Bleeding", "Bruising", "Rash", "Joint_aches", "Muscle_aches", "Dark_urine", "Jaundice"]
+        input_features = [1 if symptom in selected_symptoms else 0 for symptom in all_symptoms]
+        final = np.array(input_features, dtype=float)
+        out = model.predict([final])[0]
+        prediction_made = True  # Set the prediction flag
 
+    return render_template('pathogen_predict.html', pred=' Answer:  {}'.format(out), prediction_made=prediction_made)
 
 if __name__ == '__main__':
     app.run(debug=True)
